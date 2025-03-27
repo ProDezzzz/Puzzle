6cd1b092c5b9273b3123fb566b5c1e20312e6e27
ProDezzzz <edward.rh.nandez@gmail.com>
2025-03-27 20:49:14 +0700

    Update sounds.js

--- sounds.js
+++ sounds.js
@@ -1,6 +1,6 @@
 
 const sounds = {
-  start: new Audio('sounds/arcade-ui-6-229503.mp3'),
-  win: new Audio('sounds/gamestart-272829.mp3'),
-  switch: new Audio('sounds/light-switch-156813.mp3')
+  start: new Audio('arcade-ui-6-229503.mp3'),
+  win: new Audio('gamestart-272829.mp3'),
+  switch: new Audio('light-switch-156813.mp3')
 };
